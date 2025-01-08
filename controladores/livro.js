d6e20d21649bcd8726d103c1ebf4b9abee28396f
ale-simoes12 <alessandro.medrano321@gmail.com>
2025-01-08 15:36:28 -0300

    tratanto erros

--- controladores/livro.js
+++ controladores/livro.js
@@ -22,7 +22,14 @@ function getLivro(req, res) {
         const id = req.params.id
         const fs = require("fs")
         const livro = getLivroById(id)
-        res.send(livro)
+        if(livro===-1){
+            res.status(400)
+            res.send("Erro: ID do livro não encontrado.");            
+        }
+        else{
+            res.send(livro)
+        }
+       
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -34,12 +41,25 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const new_book = req.body
-        insereLivro(new_book)
-        res.status(201)
+        const novo_livro  = insereLivro(new_book)
+
+        
+
+        if(novo_livro === 1){
+            res.status(201)
+            res.send("livro inserido")
+        }
+
+        else if (novo_livro === -1) {
+            res.status(400); // Código de erro para requisição inválida
+            res.send("Erro: ID do livro já está em uso.");
+                
+        }
+        
     } catch (error) {
         res.status(500)
         res.send(error.message)
-        res.send("livro inserido")
+        
     } 
 }
 
@@ -62,8 +82,16 @@ function deletaLivro(req, res) {
     try {
         const id = req.params.id
         
-        deletarLivro(id)
-        res.send("Item deletado com sucesso")
+        const deletar = deletarLivro(id)
+        if(deletar === -1){
+            res.status(400)
+            res.send("Erro: ID do livro não encontrado.");   
+
+        }
+        else{
+            res.send("Item deletado com sucesso")
+        }
+        
     } catch (error) {
         res.status(500)
         res.send(error.message)
--- servicos/livro.js
+++ servicos/livro.js
@@ -4,19 +4,36 @@ const { json } = require("stream/consumers")
 function getTodosLivros() {
     return JSON.parse( fs.readFileSync("livros.json") )
 }
+
+
+
 function  getLivroById(id) {
     const livros = JSON.parse( fs.readFileSync("livros.json"))
-    const livro_filtrado = livros.filter(livro => livro.id == id)[0]
-    console.log(livro_filtrado)
-    return livro_filtrado
+    const idExiste = livros.some(livro => livro.id === id);
+    if(idExiste){
+        const livro_filtrado = livros.filter(livro => livro.id == id)[0]
+        console.log(livro_filtrado)
+        return livro_filtrado
+    }
+    else{
+        return -1
+    }
 
 }
 
 function insereLivro(new_book){
     const livros = JSON.parse( fs.readFileSync("livros.json"))
+    const idExiste = livros.some(livro => livro.id === new_book.id);
+
+    if(idExiste){
+    return -1
+    }
+    else{
+
     const novaListadeLivros =  [...livros,new_book]
     fs.writeFileSync("livros.json" , JSON.stringify(novaListadeLivros) )
-
+    return 1
+    }
        
 
 }
@@ -34,10 +51,18 @@ function modificaLivro(modificacoes , id) {
 function deletarLivro(id){
     let livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
     const indiceModificado = livrosAtuais.findIndex(livro => livro.id === id)
-    livrosAtuais.splice(indiceModificado,1);
-    fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
-   
     
+
+    if(indiceModificado === -1){
+        return -1
+    }
+    else{
+        livrosAtuais.splice(indiceModificado,1);
+        fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
+    }
+
+
+   
 
 }
 
